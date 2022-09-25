e8df377391ef385af960e348e423982e2176c8d6
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-25 21:14:34 +0530

    Add: Creating A simple Linked List

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -3,9 +3,9 @@ package com.bridgelabz.DataStructures;
 public class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedList<Integer> list1 = new LinkedList<>();
-		list1.push(70);
-		list1.push(30);
 		list1.push(56);
+		list1.push(30);
+		list1.push(70);
 		list1.show();
 	}
 }
