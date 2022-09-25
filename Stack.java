e442b0558b1650c8957c313f24ae42bf4c66773d
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-25 22:53:49 +0530

    Add: Ability To Create A Stack

---
+++ Stack.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.DataStructures;
+
+public class Stack {
+	public static void main(String[] args) {
+		LinkedList<Integer> stack = new LinkedList<>();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		stack.show();
+	}
+}
