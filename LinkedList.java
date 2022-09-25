7048e5136c5f4030eba923fe172c8719167d8380
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-25 21:34:27 +0530

    Add: Inserting Element In Between Linked List

--- LinkedList.java
+++ LinkedList.java
@@ -15,6 +15,20 @@ public class LinkedList<T> {
 		}
 	}
 
+	public void insert(T data) {
+		Node<T> newNode = new Node<>(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			Node<T> temp, current;
+			current = head;
+			temp = head.next;
+			current.next = newNode;
+			newNode.next = temp;
+		}
+	}
+
 	public void add(T data) {
 		Node<T> newNode = new Node<>(data);
 		if (head == null) {
--- LinkedListMain.java
+++ LinkedListMain.java
@@ -4,14 +4,16 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedList<Integer> list1 = new LinkedList<>();
 		list1.push(70);
-		list1.push(30);
 		list1.push(56);
+		list1.insert(30);
 		list1.show();
+
+		System.out.println("\n");
 
 		LinkedList<Integer> list2 = new LinkedList<>();
 		list2.add(56);
-		list2.add(30);
 		list2.add(70);
+		list2.insert(30);
 		list2.show();
 	}
 }
